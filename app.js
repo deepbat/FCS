e189d0810e201f15bd3065f9399887e6d0165a5a
Deepak Batra <deepak.batra@outlook.com>
2026-09-25 11:11:46 +0530

    Fix merged register holiday query

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ async function loadRecords(){
     db.from('employees').select('id,name,category,normal_work_minutes,break_minutes,round_minutes,split_shift').eq('active',true).order('name'),
     db.from('daily_records').select('id,work_date,in_time,out_time,in_time_2,out_time_2,worked_minutes,ot_minutes,employee_id').gte('work_date',dateStart).lt('holiday_date',end).order('work_date'),
     db.from('attendance').select('work_date,employee_id,status').gte('work_date',dateStart).lt('work_date',end),
-    db.from('holidays').select('holiday_date,name').gte('holiday_date',dateStart).lt('work_date',end)
+    db.from('holidays').select('holiday_date,name').gte('holiday_date',dateStart).lt('holiday_date',end)
   ]);
   if(error){$('recordsTable').innerHTML='<tr><td>'+esc(error.message)+'</td></tr>';return}
   const list=emps||[];
